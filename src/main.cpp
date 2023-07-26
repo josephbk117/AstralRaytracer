ea297d64c7134ad6c311f110f34a4438f4f4b4c9
Joseph Kalathil <josephbk.117@hotmail.com>
2023-07-27 01:38:20 +0200

    Made window run on windows & kali linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "../includes/WindowFramework/Window.h"
-#include "../includes/WindowFramework/WindowUtils.h"
+#include "WindowFramework/Window.h"
+#include "WindowFramework/WindowUtils.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
@@ -8,5 +9,30 @@ int main()
     Window window("new window test");
 
     std::cout<<"\n window name :"<<window.getName()<<"\nMax window count "<<WindowUtils::GetMaxWindowsAllowed();
+
+    GLFWwindow* glfwWindow = nullptr;
+    int width = 300, height = 300;
+
+    if(!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindow = glfwCreateWindow(width, height, "Custom Window", nullptr, nullptr);
+
+    if(!glfwWindow)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while(!glfwWindowShouldClose(glfwWindow))
+    {
+        glfwSwapBuffers(glfwWindow);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
