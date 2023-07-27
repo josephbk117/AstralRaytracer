cd8fd4e49f83eeec548700910e74eb4143897f4d
Joseph Kalathil <josephbk.117@hotmail.com>
2023-07-27 19:03:53 +0200

    added open gl to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "WindowFramework/Window.h"
 #include "WindowFramework/WindowUtils.h"
+#define GLFW_INCLUDE_NONE
+#include <glbinding/gl/gl.h>
+#include <glbinding/glbinding.h>
 #include "GLFW/glfw3.h"
+
 
 int main()
 {
@@ -18,6 +22,10 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     glfwWindow = glfwCreateWindow(width, height, "Custom Window", nullptr, nullptr);
 
     if(!glfwWindow)
@@ -26,13 +34,19 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+    glfwMakeContextCurrent(glfwWindow);
+    glbinding::initialize(glfwGetProcAddress);
+
+    gl::glViewport(0,0,width,height);
+
     while(!glfwWindowShouldClose(glfwWindow))
     {
+		gl::glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		gl::glClear(gl::GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(glfwWindow);
         glfwPollEvents();
     }
 
     glfwTerminate();
-
     return 0;
 }
