8a0a2af760913f318b1913f6b4b65f7565ee65b3
joseph <josephbk.117@hotmail.com>
2023-08-01 18:38:55 +0200

    Added Triangle Traceable

---
+++ includes/Raytracer/Traceable/TriangleTraceable.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Traceable.h"
+
+namespace AstralRaytracer
+{
+	class TriangleTraceable : public Traceable
+	{
+	public:
+		TriangleTraceable();
+		TriangleTraceable(const glm::vec3& vertexA, const glm::vec3& vertexB, const glm::vec3& vertexC);
+		float32 getTriangleDistance() const;
+		bool trace(const Ray& rayIn, HitInfo& hitInfo) override;
+
+	private:
+		glm::vec3 m_vertexA, m_vertexB, m_vertexC;
+		glm::vec3 m_normal;
+	};
+
+}
