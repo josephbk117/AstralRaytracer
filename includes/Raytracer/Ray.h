7b8bef993ba70c390e4599b4a7e01ebbb0430c6a
Joseph Kalathil <josephbk.117@hotmail.com>
2023-08-09 16:05:52 +0200

    Updated names of Ray struct members

--- includes/Raytracer/Ray.h
+++ includes/Raytracer/Ray.h
@@ -6,7 +6,7 @@
 
 struct Ray
 {
-	glm::vec3 origin;
+	glm::vec3 worldSpacePosition;
 	glm::vec3 direction;
 };
 
@@ -14,6 +14,6 @@ struct HitInfo
 {
 	float32   hitDistance  = std::numeric_limits<float32>::max();
 	uint32    materialIndex= 0;
-	glm::vec3 normal;
+	glm::vec3 worldSpaceNormal;
 	Ray       rayOut;
 };
