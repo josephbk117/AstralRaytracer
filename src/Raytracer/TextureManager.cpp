93a1a32e5c6e597ece1c23995b1575f1e87757bf
Joseph Kalathil <josephbk.117@hotmail.com>
2023-08-18 19:00:17 +0200

    Updated creating textures with ALIGNMENT of 1

--- src/Raytracer/TextureManager.cpp
+++ src/Raytracer/TextureManager.cpp
@@ -5,33 +5,25 @@
 #include <iostream>
 uint32 TextureManager::loadTextureFromData(TextureData& textureData, bool gamma)
 {
-	using namespace gl;
 	uint32 textureID;
-	glGenTextures(1, &textureID);
+	gl::glGenTextures(1, &textureID);
 	const uint8* const data= textureData.getTextureData().data();
 	if(data)
 	{
-		GLenum format= GL_RED;
-		if(textureData.getComponentCount() == 1)
-			format= GL_RED;
-		else if(textureData.getComponentCount() == 3)
-			format= GL_RGB;
-		else if(textureData.getComponentCount() == 4)
-			format= GL_RGBA;
-		glBindTexture(GL_TEXTURE_2D, textureID);
-		glTexImage2D(GL_TEXTURE_2D, 0, format, textureData.getWidth(), textureData.getHeight(), 0,
-								 format, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
+		gl::glPixelStorei(gl::GL_UNPACK_ALIGNMENT, 1);
+		gl::glBindTexture(gl::GL_TEXTURE_2D, textureID);
+		gl::glTexImage2D(gl::GL_TEXTURE_2D, 0, gl::GL_RGB, textureData.getWidth(),
+										 textureData.getHeight(), 0, gl::GL_RGB, gl::GL_UNSIGNED_BYTE, data);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_WRAP_S, gl::GL_CLAMP_TO_EDGE);
+		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_WRAP_T, gl::GL_CLAMP_TO_EDGE);
+		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_MIN_FILTER, gl::GL_NEAREST);
+		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_MAG_FILTER, gl::GL_NEAREST);
 	}
-#if _DEBUG
-	else
+	else 
+	{
 		std::cout << "\nTexture failed to load with texture data " << std::endl;
-#endif
+	}
 	return textureID;
 }
 
@@ -44,10 +36,12 @@ void TextureManager::updateTexture(const TextureData& textureData, uint32 textur
 	gl::glBindTexture(gl::GL_TEXTURE_2D, 0);
 }
 
-void TextureManager::resizeTexture(uint32 textureId, uint32 width, uint32 height) 
+void TextureManager::resizeTexture(const TextureData& textureData, uint32 textureId)
 {
 	gl::glBindTexture(gl::GL_TEXTURE_2D, textureId);
-	gl::glTexImage2D(gl::GL_TEXTURE_2D, 0, gl::GL_RGBA, width, height, 0, gl::GL_RGBA, gl::GL_UNSIGNED_BYTE, nullptr);
+	gl::glTexImage2D(gl::GL_TEXTURE_2D, 0, gl::GL_RGB, textureData.getWidth(),
+									 textureData.getHeight(), 0, gl::GL_RGB, gl::GL_UNSIGNED_BYTE,
+									 textureData.getTextureData().data());
 	gl::glBindTexture(gl::GL_TEXTURE_2D, 0);
 }
 
