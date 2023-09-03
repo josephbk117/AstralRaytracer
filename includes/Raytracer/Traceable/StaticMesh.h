9f512648f5011bac73261db8e3a5bbe0ea86ffbb
Joseph Kalathil <josephbk.117@hotmail.com>
2023-09-04 00:54:15 +0200

    Added Bounding box check before ray tracing

--- includes/Raytracer/Traceable/StaticMesh.h
+++ includes/Raytracer/Traceable/StaticMesh.h
@@ -6,16 +6,28 @@
 
 namespace AstralRaytracer
 {
+	struct AABB
+	{
+		glm::vec3 min;
+		glm::vec3 max;
+	};
+
 	class StaticMesh: public Traceable
 	{
 		public:
 		StaticMesh()= default;
-		StaticMesh(const std::vector<TriangleTraceable>& triangles): m_triangles(triangles) {}
+		StaticMesh(const std::vector<TriangleTraceable>& triangles, const AABB& boundingBox)
+				: m_triangles(triangles), m_boundingBox(boundingBox)
+		{
+		}
 		bool trace(const Ray& rayIn, HitInfo& hitInfo) const override;
 		void setPosition(const glm::vec3& position) override;
 		void setMaterialIndex(uint32 materialIndex) override;
 
 		private:
+		bool intersectsBoundingBox(const Ray& rayIn) const;
+
+		AABB m_boundingBox;
 		std::vector<TriangleTraceable> m_triangles;
 	};
 } // namespace AstralRaytracer
--- src/Raytracer/Traceable/StaticMesh.cpp
+++ src/Raytracer/Traceable/StaticMesh.cpp
@@ -5,6 +5,11 @@ namespace AstralRaytracer
 
 	bool StaticMesh::trace(const Ray& rayIn, HitInfo& hitInfo) const
 	{
+		if(!intersectsBoundingBox(rayIn))
+		{
+			return false;
+		}
+
 		HitInfo closestHitInfo;
 		for(uint32 triIndex= 0; triIndex < m_triangles.size(); ++triIndex)
 		{
@@ -26,7 +31,7 @@ namespace AstralRaytracer
 		return false;
 	}
 
-void StaticMesh::setPosition(const glm::vec3& position)
+	void StaticMesh::setPosition(const glm::vec3& position)
 	{
 		for(uint32 triIndex= 0; triIndex < m_triangles.size(); ++triIndex)
 		{
@@ -36,12 +41,49 @@ void StaticMesh::setPosition(const glm::vec3& position)
 		Traceable::setPosition(position);
 	}
 
-void StaticMesh::setMaterialIndex(uint32 materialIndex)
+	void StaticMesh::setMaterialIndex(uint32 materialIndex)
 	{
 		for(uint32 triIndex= 0; triIndex < m_triangles.size(); ++triIndex)
 		{
 			m_triangles[triIndex].setMaterialIndex(materialIndex);
 		}
+	}
+
+	bool StaticMesh::intersectsBoundingBox(const Ray& rayIn) const
+	{
+		const float32 dirFracX= 1.0f / rayIn.direction.x;
+		const float32 dirFracY= 1.0f / rayIn.direction.y;
+		const float32 dirFracZ= 1.0f / rayIn.direction.z;
+		// lb is the corner of AABB with minimal coordinates - left bottom, rt is maximal corner
+		// r.org is origin of ray
+
+		const glm::vec3& lb= m_transform.getPosition() + m_boundingBox.min;
+		const glm::vec3& rt= m_transform.getPosition() + m_boundingBox.max;
+
+		const float32 t1= (lb.x - rayIn.worldSpacePosition.x) * dirFracX;
+		const float32 t2= (rt.x - rayIn.worldSpacePosition.x) * dirFracX;
+		const float32 t3= (lb.y - rayIn.worldSpacePosition.y) * dirFracY;
+		const float32 t4= (rt.y - rayIn.worldSpacePosition.y) * dirFracY;
+		const float32 t5= (lb.z - rayIn.worldSpacePosition.z) * dirFracZ;
+		const float32 t6= (rt.z - rayIn.worldSpacePosition.z) * dirFracZ;
+
+		const float32 tmax= glm::min(glm::min(glm::max(t1, t2), glm::max(t3, t4)), glm::max(t5, t6));
+
+		// if tmax < 0, ray (line) is intersecting AABB, but the whole AABB is behind us
+		if(tmax < 0.0f)
+		{
+			return false;
+		}
+
+		const float32 tmin= glm::max(glm::max(glm::min(t1, t2), glm::min(t3, t4)), glm::min(t5, t6));
+
+		// if tmin > tmax, ray doesn't intersect AABB
+		if(tmin > tmax)
+		{
+			return false;
+		}
+
+		return true;
 	}
 
 } // namespace AstralRaytracer
