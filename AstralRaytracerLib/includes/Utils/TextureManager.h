5ea774fdedb69a6766d4a49e24929a28ce3275bf
Joseph Kalathil <josephbk.117@hotmail.com>
2024-03-08 20:44:57 +0100

    made texture manager completely generic

--- AstralRaytracerLib/includes/Utils/TextureManager.h
+++ AstralRaytracerLib/includes/Utils/TextureManager.h
@@ -13,6 +13,17 @@ namespace AstralRaytracer
 			TextureManager()                     = delete;
 			TextureManager(const TextureManager&)= delete;
 
+			[[nodiscard]]
+			static constexpr gl::GLenum getTextureFormatFromComponentCount(uint32 componentCount);
+
+			template<ArithMeticType T>
+			[[nodiscard]]
+			static gl::GLenum getTextureDataType();
+
+			template<ArithMeticType T>
+			[[nodiscard]]
+			static gl::GLenum getTextureInternalFormatType(uint32 componentCount);
+
 			template<ArithMeticType T, uint32 componentCount>
 			static void loadTextureDataFromFile(
 					const std::filesystem::path&    path,
@@ -20,21 +31,26 @@ namespace AstralRaytracer
 					bool                            gamma
 			);
 
+			template<ArithMeticType T, uint32 componentCount>
 			[[nodiscard]]
-			static uint32 loadTextureFromTextureData(TextureDataRGBF& textureData, bool gamma);
-			static void   updateTexture(const TextureDataRGBF& textureData, uint32 textureId);
-			static void   resizeTexture(const TextureDataRGBF& textureData, uint32 textureId);
+			static uint32
+			loadTextureFromTextureData(const TextureData<T, componentCount>& textureData, bool gamma);
+
+			template<ArithMeticType T, uint32 componentCount>
+			static void
+			updateTexture(const TextureData<T, componentCount>& textureData, uint32 textureId);
+			template<ArithMeticType T, uint32 componentCount>
+			static void
+			resizeTexture(const TextureData<T, componentCount>& textureData, uint32 textureId);
 			static void saveTextureToFile(const TextureDataRGBF& data, const std::filesystem::path& path);
-			[[nodiscard]]
-			static gl::GLenum getTextureFormatFromData(TextureDataRGBF& textureData);
-			[[nodiscard]]
-			static gl::GLenum getTextureFormatFromData(uint32 componentCount);
 		private:
+			template<ArithMeticType T>
 			static void loadTextureFromRawData(
-					const float32* const data,
-					uint32               width,
-					uint32               height,
-					uint32               textureID
+					const T* const data,
+					uint32         width,
+					uint32         height,
+					uint32         componentCount,
+					uint32         textureID
 			);
 	};
 
--- AstralRaytracerLib/src/Utils/TextureManager.cpp
+++ AstralRaytracerLib/src/Utils/TextureManager.cpp
@@ -1,7 +1,5 @@
 #include "Utils/TextureManager.h"
 
-#include "Utils/TextureData.h"
-
 #include <stbimage/stb_image.h>
 #include <stbimage/stb_image_write.h>
 
@@ -11,6 +9,137 @@
 
 namespace AstralRaytracer
 {
+	constexpr gl::GLenum TextureManager::getTextureFormatFromComponentCount(uint32 componentCount)
+	{
+		ASTRAL_ASSERTM(componentCount <= 4, "Invalid component count requested");
+
+		switch(componentCount)
+		{
+			case 1: return gl::GLenum::GL_RED;
+			case 2: return gl::GLenum::GL_RG;
+			case 3: return gl::GLenum::GL_RGB;
+			case 4: return gl::GLenum::GL_RGBA;
+		}
+
+		return gl::GLenum::GL_INVALID_ENUM;
+	}
+
+	template<ArithMeticType T>
+	gl::GLenum TextureManager::getTextureDataType()
+	{
+		if(std::is_same_v<T, float32>)
+		{
+			return gl::GLenum::GL_FLOAT;
+		}
+		else if(std::is_same_v<T, uint8>)
+		{
+			return gl::GLenum::GL_UNSIGNED_BYTE;
+		}
+		else if(std::is_same_v<T, int8>)
+		{
+			return gl::GLenum::GL_BYTE;
+		}
+		else if(std::is_same_v<T, uint16>)
+		{
+			return gl::GLenum::GL_UNSIGNED_SHORT;
+		}
+		else if(std::is_same_v<T, int16>)
+		{
+			return gl::GLenum::GL_SHORT;
+		}
+		else if(std::is_same_v<T, uint32>)
+		{
+			return gl::GLenum::GL_UNSIGNED_INT;
+		}
+		else if(std::is_same_v<T, int32>)
+		{
+			return gl::GLenum::GL_INT;
+		}
+
+		static_assert(true, "Unsupported type");
+	}
+
+	template<ArithMeticType T>
+	gl::GLenum TextureManager::getTextureInternalFormatType(uint32 componentCount)
+	{
+		ASTRAL_ASSERTM(componentCount <= 4, "Invalid component count requested");
+
+		if(std::is_same_v<T, float32>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R32F;
+				case 2: return gl::GLenum::GL_RG32F;
+				case 3: return gl::GLenum::GL_RGB32F;
+				case 4: return gl::GLenum::GL_RGBA32F;
+			}
+		}
+		else if(std::is_same_v<T, uint8>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R8;
+				case 2: return gl::GLenum::GL_RG8;
+				case 3: return gl::GLenum::GL_RGB8;
+				case 4: return gl::GLenum::GL_RGBA8;
+			}
+		}
+		else if(std::is_same_v<T, int8>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R8I;
+				case 2: return gl::GLenum::GL_RG8I;
+				case 3: return gl::GLenum::GL_RGB8I;
+				case 4: return gl::GLenum::GL_RGBA8I;
+			}
+		}
+		else if(std::is_same_v<T, uint16>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R16;
+				case 2: return gl::GLenum::GL_RG16;
+				case 3: return gl::GLenum::GL_RGB16;
+				case 4: return gl::GLenum::GL_RGBA16;
+			}
+		}
+		else if(std::is_same_v<T, int16>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R16I;
+				case 2: return gl::GLenum::GL_RG16I;
+				case 3: return gl::GLenum::GL_RGB16I;
+				case 4: return gl::GLenum::GL_RGBA16I;
+			}
+		}
+		else if(std::is_same_v<T, uint32>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R32UI;
+				case 2: return gl::GLenum::GL_RG32UI;
+				case 3: return gl::GLenum::GL_RGB32UI;
+				case 4: return gl::GLenum::GL_RGBA32UI;
+			}
+		}
+		else if(std::is_same_v<T, int32>)
+		{
+			switch(componentCount)
+			{
+				case 1: return gl::GLenum::GL_R32I;
+				case 2: return gl::GLenum::GL_RG32I;
+				case 3: return gl::GLenum::GL_RGB32I;
+				case 4: return gl::GLenum::GL_RGBA32I;
+			}
+		}
+
+		static_assert(true, "Unsupported type");
+
+		return gl::GLenum::GL_INVALID_ENUM;
+	}
+
 	template<ArithMeticType T, uint32 componentCount>
 	void TextureManager::loadTextureDataFromFile(
 			const std::filesystem::path&    path,
@@ -89,21 +218,30 @@ namespace AstralRaytracer
 		stbi_image_free(data);
 	}
 
-	uint32 TextureManager::loadTextureFromTextureData(TextureDataRGBF& textureData, bool gamma)
+	template<ArithMeticType T, uint32 componentCount>
+	[[nodiscard]]
+	uint32 TextureManager::loadTextureFromTextureData(
+			const TextureData<T, componentCount>& textureData,
+			bool                                  gamma
+	)
 	{
 		uint32 textureID;
 		gl::glGenTextures(1, &textureID);
-		const float32* const data= textureData.getTextureData().data();
+		const T* const data= textureData.getTextureData().data();
 		assert(data);
-		loadTextureFromRawData(data, textureData.getWidth(), textureData.getHeight(), textureID);
+		loadTextureFromRawData(
+				data, textureData.getWidth(), textureData.getHeight(), componentCount, textureID
+		);
 		return textureID;
 	}
 
+	template<ArithMeticType T>
 	void TextureManager::loadTextureFromRawData(
-			const float32* const data,
-			uint32               width,
-			uint32               height,
-			uint32               textureID
+			const T* const data,
+			uint32         width,
+			uint32         height,
+			uint32         componentCount,
+			uint32         textureID
 	)
 	{
 		if(data == nullptr)
@@ -114,33 +252,53 @@ namespace AstralRaytracer
 
 		gl::glPixelStorei(gl::GL_UNPACK_ALIGNMENT, 1);
 		gl::glBindTexture(gl::GL_TEXTURE_2D, textureID);
-		gl::glTexImage2D(
-				gl::GL_TEXTURE_2D, 0, gl::GL_RGB32F, width, height, 0, gl::GL_RGB, gl::GL_FLOAT, data
-		);
 
+		const gl::GLenum format        = getTextureFormatFromComponentCount(componentCount);
+		const gl::GLenum dataType      = getTextureDataType<T>();
+		const gl::GLenum internalFormat= getTextureFormatFromComponentCount(componentCount);
+
+		gl::glTexImage2D(gl::GL_TEXTURE_2D, 0, gl::GL_RGB32F, width, height, 0, format, dataType, data);
 		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_WRAP_S, gl::GL_CLAMP_TO_EDGE);
 		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_WRAP_T, gl::GL_CLAMP_TO_EDGE);
 		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_MIN_FILTER, gl::GL_NEAREST);
 		gl::glTexParameteri(gl::GL_TEXTURE_2D, gl::GL_TEXTURE_MAG_FILTER, gl::GL_NEAREST);
 	}
 
-	void TextureManager::updateTexture(const TextureDataRGBF& textureData, uint32 textureId)
+	template<ArithMeticType T, uint32 componentCount>
+	void
+	TextureManager::updateTexture(const TextureData<T, componentCount>& textureData, uint32 textureId)
 	{
 		gl::glBindTexture(gl::GL_TEXTURE_2D, textureId);
+
+		const int32 texWidth = textureData.getWidth();
+		const int32 texHeight= textureData.getHeight();
+
+		constexpr gl::GLenum format  = getTextureFormatFromComponentCount(componentCount);
+		const gl::GLenum     dataType= getTextureDataType<T>();
+
 		gl::glTexSubImage2D(
-				gl::GL_TEXTURE_2D, 0, 0, 0, textureData.getWidth(), textureData.getHeight(),
-				TextureManager::getTextureFormatFromData(textureData.getComponentCount()), gl::GL_FLOAT,
+				gl::GL_TEXTURE_2D, 0, 0, 0, texWidth, texHeight, format, dataType,
 				textureData.getTextureData().data()
 		);
 		gl::glBindTexture(gl::GL_TEXTURE_2D, 0);
 	}
 
-	void TextureManager::resizeTexture(const TextureDataRGBF& textureData, uint32 textureId)
+	template<ArithMeticType T, uint32 componentCount>
+	void
+	TextureManager::resizeTexture(const TextureData<T, componentCount>& textureData, uint32 textureId)
 	{
 		gl::glBindTexture(gl::GL_TEXTURE_2D, textureId);
+
+		const int32 texWidth = textureData.getWidth();
+		const int32 texHeight= textureData.getHeight();
+
+		constexpr gl::GLenum format        = getTextureFormatFromComponentCount(componentCount);
+		const gl::GLenum     dataType      = getTextureDataType<T>();
+		const gl::GLenum     internalFormat= getTextureInternalFormatType<T>(componentCount);
+
 		gl::glTexImage2D(
-				gl::GL_TEXTURE_2D, 0, gl::GL_RGB32F, textureData.getWidth(), textureData.getHeight(), 0,
-				gl::GL_RGB, gl::GL_FLOAT, textureData.getTextureData().data()
+				gl::GL_TEXTURE_2D, 0, internalFormat, texWidth, texHeight, 0, format, dataType,
+				textureData.getTextureData().data()
 		);
 		gl::glBindTexture(gl::GL_TEXTURE_2D, 0);
 	}
@@ -155,46 +313,52 @@ namespace AstralRaytracer
 		);
 	}
 
-	gl::GLenum TextureManager::getTextureFormatFromData(TextureDataRGBF& textureData)
-	{
-		return getTextureFormatFromData(textureData.getComponentCount());
-	}
+	template gl::GLenum TextureManager::getTextureDataType<uint8>();
+	template gl::GLenum TextureManager::getTextureDataType<float32>();
 
-	gl::GLenum TextureManager::getTextureFormatFromData(uint32 componentCount)
-	{
-		ASTRAL_ASSERTM(componentCount <= 4, "Invalid component count requested");
+	template void
+	TextureManager::loadTextureFromRawData<uint8>(const uint8* const, uint32, uint32, uint32, uint32);
+	template void TextureManager::loadTextureFromRawData<
+			float32>(const float32* const, uint32, uint32, uint32, uint32);
 
-		switch(componentCount)
-		{
-			case 1: return gl::GLenum::GL_RED;
-			case 2: return gl::GLenum::GL_RG;
-			case 3: return gl::GLenum::GL_RGB;
-			case 4: return gl::GLenum::GL_RGBA;
-		}
+	template uint32
+	TextureManager::loadTextureFromTextureData<uint8, 3>(const TextureData<uint8, 3>&, bool);
+	template uint32
+	TextureManager::loadTextureFromTextureData<uint8, 4>(const TextureData<uint8, 4>&, bool);
+	template uint32
+	TextureManager::loadTextureFromTextureData<float32, 3>(const TextureData<float32, 3>&, bool);
+	template uint32
+	TextureManager::loadTextureFromTextureData<float32, 4>(const TextureData<float32, 4>&, bool);
 
-		return gl::GLenum::GL_INVALID_ENUM;
-	}
-
-	template void AstralRaytracer::TextureManager::loadTextureDataFromFile<uint8, 3>(
+	template void TextureManager::loadTextureDataFromFile<uint8, 3>(
 			const std::filesystem::path&,
 			TextureData<uint8, 3>&,
 			bool
 	);
-
-	template void AstralRaytracer::TextureManager::loadTextureDataFromFile<uint8, 4>(
+	template void TextureManager::loadTextureDataFromFile<uint8, 4>(
 			const std::filesystem::path&,
 			TextureData<uint8, 4>&,
 			bool
 	);
-	template void AstralRaytracer::TextureManager::loadTextureDataFromFile<float32, 3>(
+	template void TextureManager::loadTextureDataFromFile<float32, 3>(
 			const std::filesystem::path&,
 			TextureData<float32, 3>&,
 			bool
 	);
-	template void AstralRaytracer::TextureManager::loadTextureDataFromFile<float32, 4>(
+	template void TextureManager::loadTextureDataFromFile<float32, 4>(
 			const std::filesystem::path&,
 			TextureData<float32, 4>&,
 			bool
 	);
+
+	template void TextureManager::updateTexture(const TextureData<uint8, 3>&, uint32);
+	template void TextureManager::updateTexture(const TextureData<uint8, 4>&, uint32);
+	template void TextureManager::updateTexture(const TextureData<float32, 3>&, uint32);
+	template void TextureManager::updateTexture(const TextureData<float32, 4>&, uint32);
+
+	template void TextureManager::resizeTexture(const TextureData<uint8, 3>&, uint32);
+	template void TextureManager::resizeTexture(const TextureData<uint8, 4>&, uint32);
+	template void TextureManager::resizeTexture(const TextureData<float32, 3>&, uint32);
+	template void TextureManager::resizeTexture(const TextureData<float32, 4>&, uint32);
 
 } // namespace AstralRaytracer
