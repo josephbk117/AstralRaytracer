1088ce46655ce3348e04b08c47291088f5fa2350
Joseph Kalathil <josephbk.117@hotmail.com>
2024-03-15 14:38:21 +0100

    brought shader program under astral raytracer namespace + minor refactor to reduce code cuplication

--- AstralRaytracerLib/includes/Utils/ShaderProgram.h
+++ AstralRaytracerLib/includes/Utils/ShaderProgram.h
@@ -6,65 +6,82 @@
 #include <unordered_map>
 #include <variant>
 
-using UniformDataType= std::variant<
-		int32,
-		uint32,
-		float32,
-		glm::ivec2,
-		glm::uvec2,
-		glm::vec2,
-		glm::ivec3,
-		glm::uvec3,
-		glm::vec3,
-		glm::mat4>;
-
-struct UniformData
+namespace AstralRaytracer
 {
-		uint32          uniformId= 0;
-		float32         min      = 0.0f;
-		float32         max      = 1.0f;
-		UniformDataType data;
-};
+	using UniformDataType= std::variant<
+			int32,
+			uint32,
+			float32,
+			glm::ivec2,
+			glm::uvec2,
+			glm::vec2,
+			glm::ivec3,
+			glm::uvec3,
+			glm::vec3,
+			glm::mat4>;
 
-class ShaderProgram
-{
-	public:
-		ShaderProgram();
-		~ShaderProgram();
-		void compileShaders(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
-		void compileShadersFromSrcCode(
-				const std::string& vertexShaderSrcCode,
-				const std::string& fragmentShaderSrcCode
-		);
-		void linkShaders();
-		void addAttribute(const std::string& attributeName);
-		void use() const;
-		void unuse() const;
+	struct UniformData
+	{
+			uint32          uniformId= 0;
+			float32         min      = 0.0f;
+			float32         max      = 1.0f;
+			UniformDataType data;
+	};
 
-		[[nodiscard]]
-		const std::unordered_map<std::string, UniformData>& getUniformData() const;
-		[[nodiscard]]
-		gl::GLint getUniformLocation(const std::string& uniformName) const;
+	class ShaderProgram
+	{
+		public:
+			ShaderProgram() = default;
+			~ShaderProgram()= default;
+			void
+			compileShaders(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+			void compileShadersFromSrcCode(
+					const std::string& vertexShaderSrcCode,
+					const std::string& fragmentShaderSrcCode
+			);
+			void linkShaders();
+			void addAttribute(const std::string& attributeName);
+			void use() const;
+			void unuse() const;
 
-		void setUniformValue(
-				const std::string&     uniformName,
-				const UniformDataType& data,
-				float32                min,
-				float32                max
-		);
-		void        updateUniformData(const std::string& uniformName, const UniformDataType& data);
-		static void applyShaderUniformMatrix(int32 uniformId, const glm::mat4& matrixValue);
-		static void applyShaderVector3(int32 uniformId, const glm::vec3& value);
-		static void applyShaderFloat(int32 uniformId, float32 value);
-		static void applyShaderInt(int32 uniformId, int32 value);
-		static void applyShaderInt2(int32 uniformId, const glm::ivec2& value);
-		static void applyShaderUInt(int32 uniformId, uint32 value);
-		static void applyShaderUInt2(int32 uniformId, const glm::uvec2& value);
-		static void applyShaderBool(int32 uniformId, bool value);
-	private:
-		uint32                                       programID, vertexShaderID, fragmentShaderID;
-		int32                                        attributeCount;
-		std::unordered_map<std::string, UniformData> m_uniformMap;
-		void compileShaderFromFilePath(const std::string& filePath, uint32 ID);
-		bool compileShaderSourceCode(const std::string& srcCode, uint32 ID);
-};
+			[[nodiscard]]
+			const std::unordered_map<std::string, UniformData>& getUniformData() const;
+			[[nodiscard]]
+			gl::GLint getUniformLocation(const std::string& uniformName) const;
+
+			void setUniformValue(
+					const std::string&     uniformName,
+					const UniformDataType& data,
+					float32                min,
+					float32                max
+			);
+			void        updateUniformData(const std::string& uniformName, const UniformDataType& data);
+			static void applyShaderUniformMatrix(int32 uniformId, const glm::mat4& matrixValue);
+			static void applyShaderVector3(int32 uniformId, const glm::vec3& value);
+			static void applyShaderFloat(int32 uniformId, float32 value);
+			static void applyShaderInt(int32 uniformId, int32 value);
+			static void applyShaderInt2(int32 uniformId, const glm::ivec2& value);
+			static void applyShaderUInt(int32 uniformId, uint32 value);
+			static void applyShaderUInt2(int32 uniformId, const glm::uvec2& value);
+			static void applyShaderBool(int32 uniformId, bool value);
+		private:
+			uint32 programID{ 0 };
+			uint32 vertexShaderID{ 0 };
+			uint32 fragmentShaderID{ 0 };
+			int32  attributeCount{ 0 };
+
+			std::unordered_map<std::string, UniformData> m_uniformMap;
+
+			[[nodiscard]]
+			uint32 createProgram(Errors::GenericError& error) const;
+			[[nodiscard]]
+			uint32 createVertexShader(Errors::GenericError& error) const;
+			[[nodiscard]]
+			uint32 createFragmentShader(Errors::GenericError& error) const;
+
+			void initShader(Errors::GenericError& error);
+
+			void compileShaderFromFilePath(const std::string& filePath, uint32 ID);
+			bool compileShaderSourceCode(const std::string& srcCode, uint32 ID);
+	};
+} // namespace AstralRaytracer
--- AstralRaytracerLib/src/Utils/ShaderProgram.cpp
+++ AstralRaytracerLib/src/Utils/ShaderProgram.cpp
@@ -2,269 +2,287 @@
 
 #include "Utils/Common.h"
 
-#include <fstream>
 #include <vector>
 
-ShaderProgram::ShaderProgram()
+namespace AstralRaytracer
 {
-	programID       = 0;
-	vertexShaderID  = 0;
-	fragmentShaderID= 0;
-	attributeCount  = 0;
-}
+	void ShaderProgram::compileShaders(
+			const std::string& vertexShaderPath,
+			const std::string& fragmentShaderPath
+	)
+	{
+		Errors::GenericError error= Errors::GenericError::INVALID;
+		initShader(error);
 
-ShaderProgram::~ShaderProgram() { }
-
-void ShaderProgram::compileShaders(
-		const std::string& vertexShaderPath,
-		const std::string& fragmentShaderPath
-)
-{
-	programID     = gl::glCreateProgram();
-	vertexShaderID= gl::glCreateShader(gl::GL_VERTEX_SHADER);
-	if(vertexShaderID == 0)
-	{
-		ASTRAL_LOG_WARN("Vertex shader creation failed");
-	}
-	fragmentShaderID= gl::glCreateShader(gl::GL_FRAGMENT_SHADER);
-	if(vertexShaderID == 0)
-	{
-		ASTRAL_LOG_WARN("Fragment shader creation failed");
-	}
-	compileShaderFromFilePath(vertexShaderPath, vertexShaderID);
-	compileShaderFromFilePath(fragmentShaderPath, fragmentShaderID);
-}
-
-void ShaderProgram::compileShadersFromSrcCode(
-		const std::string& vertexShaderSrcCode,
-		const std::string& fragmentShaderSrcCode
-)
-{
-	programID     = gl::glCreateProgram();
-	vertexShaderID= gl::glCreateShader(gl::GL_VERTEX_SHADER);
-	if(vertexShaderID == 0)
-	{
-		ASTRAL_LOG_WARN("Vertex shader creation failed");
-	}
-	fragmentShaderID= gl::glCreateShader(gl::GL_FRAGMENT_SHADER);
-	if(vertexShaderID == 0)
-	{
-		ASTRAL_LOG_WARN("Fragment shader creation failed");
+		compileShaderFromFilePath(vertexShaderPath, vertexShaderID);
+		compileShaderFromFilePath(fragmentShaderPath, fragmentShaderID);
 	}
 
-	if(!compileShaderSourceCode(vertexShaderSrcCode, vertexShaderID))
+	void ShaderProgram::compileShadersFromSrcCode(
+			const std::string& vertexShaderSrcCode,
+			const std::string& fragmentShaderSrcCode
+	)
 	{
-		ASTRAL_LOG_WARN("Vertex shader compilation failed");
-	}
-	if(!compileShaderSourceCode(fragmentShaderSrcCode, fragmentShaderID))
-	{
-		ASTRAL_LOG_WARN("Fragment shader compilation failed");
-	}
-}
+		Errors::GenericError error= Errors::GenericError::INVALID;
+		initShader(error);
 
-void ShaderProgram::linkShaders()
-{
-	gl::glAttachShader(programID, vertexShaderID);
-	gl::glAttachShader(programID, fragmentShaderID);
+		if(!compileShaderSourceCode(vertexShaderSrcCode, vertexShaderID))
+		{
+			ASTRAL_LOG_ERROR("Vertex shader compilation failed");
+		}
+		if(!compileShaderSourceCode(fragmentShaderSrcCode, fragmentShaderID))
+		{
+			ASTRAL_LOG_ERROR("Fragment shader compilation failed");
+		}
+	}
 
-	gl::glLinkProgram(programID);
-	gl::GLint isLinked= 0;
-	gl::glGetProgramiv(programID, gl::GL_LINK_STATUS, &isLinked);
-	if(isLinked == static_cast<gl::GLint>(gl::GL_FALSE))
+	void ShaderProgram::linkShaders()
 	{
-		gl::GLint maxLength= 0;
-		gl::glGetProgramiv(programID, gl::GL_INFO_LOG_LENGTH, (int32*)&isLinked);
-		std::vector<char> infoLog(maxLength);
-		ASTRAL_LOG_DEBUG(&(infoLog[0]));
-		gl::glGetProgramInfoLog(programID, maxLength, &maxLength, &infoLog[0]);
-		gl::glDeleteProgram(programID);
+		gl::glAttachShader(programID, vertexShaderID);
+		gl::glAttachShader(programID, fragmentShaderID);
+
+		gl::glLinkProgram(programID);
+		gl::GLint isLinked= 0;
+		gl::glGetProgramiv(programID, gl::GL_LINK_STATUS, &isLinked);
+		if(isLinked == static_cast<gl::GLint>(gl::GL_FALSE))
+		{
+			gl::GLint maxLength= 0;
+			gl::glGetProgramiv(programID, gl::GL_INFO_LOG_LENGTH, (int32*)&isLinked);
+			std::vector<char> infoLog(maxLength);
+			ASTRAL_LOG_DEBUG(&(infoLog[0]));
+			gl::glGetProgramInfoLog(programID, maxLength, &maxLength, &infoLog[0]);
+			gl::glDeleteProgram(programID);
+			gl::glDeleteShader(vertexShaderID);
+			gl::glDeleteShader(fragmentShaderID);
+		}
+		else
+		{
+			ASTRAL_LOG_TRACE("Shader linked successfully");
+		}
+
+		gl::glDetachShader(programID, vertexShaderID);
+		gl::glDetachShader(programID, fragmentShaderID);
 		gl::glDeleteShader(vertexShaderID);
 		gl::glDeleteShader(fragmentShaderID);
 	}
-	else
+
+	void ShaderProgram::addAttribute(const std::string& attributeName)
 	{
-		ASTRAL_LOG_TRACE("Shader linked successfully");
+		gl::glBindAttribLocation(programID, attributeCount++, attributeName.c_str());
 	}
 
-	gl::glDetachShader(programID, vertexShaderID);
-	gl::glDetachShader(programID, fragmentShaderID);
-	gl::glDeleteShader(vertexShaderID);
-	gl::glDeleteShader(fragmentShaderID);
-}
-
-void ShaderProgram::addAttribute(const std::string& attributeName)
-{
-	gl::glBindAttribLocation(programID, attributeCount++, attributeName.c_str());
-}
-
-void ShaderProgram::use() const
-{
-	gl::glUseProgram(programID);
-	for(int32 i= 0; i < attributeCount; i++)
+	void ShaderProgram::use() const
 	{
-		gl::glEnableVertexAttribArray(i);
+		gl::glUseProgram(programID);
+		for(int32 i= 0; i < attributeCount; i++)
+		{
+			gl::glEnableVertexAttribArray(i);
+		}
 	}
-}
 
-void ShaderProgram::unuse() const
-{
-	gl::glUseProgram(0);
-	for(int32 i= 0; i < attributeCount; i++)
+	void ShaderProgram::unuse() const
 	{
-		gl::glDisableVertexAttribArray(i);
+		gl::glUseProgram(0);
+		for(int32 i= 0; i < attributeCount; i++)
+		{
+			gl::glDisableVertexAttribArray(i);
+		}
 	}
-}
 
-const std::unordered_map<std::string, UniformData>& ShaderProgram::getUniformData() const
-{
-	return m_uniformMap;
-}
-
-gl::GLint ShaderProgram::getUniformLocation(const std::string& uniformName) const
-{
-	return gl::glGetUniformLocation(programID, uniformName.c_str());
-}
-
-void ShaderProgram::setUniformValue(
-		const std::string&     uniformName,
-		const UniformDataType& data,
-		float32                min,
-		float32                max
-)
-{
-	uint32 uniformId= 0;
-
-	auto it= m_uniformMap.find(uniformName);
-	if(it != m_uniformMap.end())
+	const std::unordered_map<std::string, UniformData>& ShaderProgram::getUniformData() const
 	{
-		uniformId= it->second.uniformId;
+		return m_uniformMap;
 	}
-	else
+
+	gl::GLint ShaderProgram::getUniformLocation(const std::string& uniformName) const
 	{
-		uniformId                = getUniformLocation(uniformName);
+		return gl::glGetUniformLocation(programID, uniformName.c_str());
+	}
+
+	void ShaderProgram::setUniformValue(
+			const std::string&     uniformName,
+			const UniformDataType& data,
+			float32                min,
+			float32                max
+	)
+	{
+		uint32 uniformId= 0;
+
+		auto it= m_uniformMap.find(uniformName);
+		if(it != m_uniformMap.end())
+		{
+			uniformId= it->second.uniformId;
+		}
+		else
+		{
+			uniformId                = getUniformLocation(uniformName);
+			m_uniformMap[uniformName]= { uniformId, min, max, data };
+		}
+
 		m_uniformMap[uniformName]= { uniformId, min, max, data };
+
+		updateUniformData(uniformName, data);
 	}
 
-	m_uniformMap[uniformName]= { uniformId, min, max, data };
-
-	updateUniformData(uniformName, data);
-}
-
-void ShaderProgram::updateUniformData(const std::string& uniformName, const UniformDataType& data)
-{
-	const int32 uniformId= m_uniformMap[uniformName].uniformId;
-
-	m_uniformMap[uniformName].data= data;
-
-	using namespace AstralRaytracer;
-
-	if(std::get_if<int32>(&data))
+	void ShaderProgram::updateUniformData(const std::string& uniformName, const UniformDataType& data)
 	{
-		applyShaderInt(uniformId, std::get<int32>(data));
-		return;
+		const int32 uniformId= m_uniformMap[uniformName].uniformId;
+
+		m_uniformMap[uniformName].data= data;
+
+		using namespace AstralRaytracer;
+
+		if(std::get_if<int32>(&data))
+		{
+			applyShaderInt(uniformId, std::get<int32>(data));
+			return;
+		}
+
+		if(std::get_if<uint32>(&data))
+		{
+			applyShaderUInt(uniformId, std::get<uint32>(data));
+			return;
+		}
+
+		if(std::get_if<float32>(&data))
+		{
+			applyShaderFloat(uniformId, std::get<float32>(data));
+			return;
+		}
 	}
 
-	if(std::get_if<uint32>(&data))
+	void ShaderProgram::applyShaderUniformMatrix(int32 uniformId, const glm::mat4& matrixValue)
 	{
-		applyShaderUInt(uniformId, std::get<uint32>(data));
-		return;
+		gl::glUniformMatrix4fv(uniformId, 1, gl::GL_FALSE, &matrixValue[0][0]);
 	}
 
-	if(std::get_if<float32>(&data))
+	void ShaderProgram::applyShaderVector3(int32 uniformId, const glm::vec3& value)
 	{
-		applyShaderFloat(uniformId, std::get<float32>(data));
-		return;
+		gl::glUniform3f(uniformId, value.x, value.y, value.z);
 	}
-}
 
-void ShaderProgram::applyShaderUniformMatrix(int32 uniformId, const glm::mat4& matrixValue)
-{
-	gl::glUniformMatrix4fv(uniformId, 1, gl::GL_FALSE, &matrixValue[0][0]);
-}
-
-void ShaderProgram::applyShaderVector3(int32 uniformId, const glm::vec3& value)
-{
-	gl::glUniform3f(uniformId, value.x, value.y, value.z);
-}
-
-void ShaderProgram::applyShaderFloat(int32 uniformId, float32 value)
-{
-	gl::glUniform1f(uniformId, value);
-}
-
-void ShaderProgram::applyShaderInt(int32 uniformId, int32 value)
-{
-	gl::glUniform1i(uniformId, value);
-}
-
-void ShaderProgram::applyShaderInt2(int32 uniformId, const glm::ivec2& value)
-{
-	gl::glUniform2i(uniformId, value.x, value.y);
-}
-
-void ShaderProgram::applyShaderUInt(int32 uniformId, uint32 value)
-{
-	gl::glUniform1ui(uniformId, value);
-}
-
-void ShaderProgram::applyShaderUInt2(int32 uniformId, const glm::uvec2& value)
-{
-	gl::glUniform2ui(uniformId, value.x, value.y);
-}
-
-void ShaderProgram::applyShaderBool(int32 uniformId, bool value)
-{
-	gl::glUniform1i(uniformId, value);
-}
-
-void ShaderProgram::compileShaderFromFilePath(const std::string& filePath, uint32 ID)
-{
-	std::ifstream shaderFile(filePath);
-	if(shaderFile.fail())
+	void ShaderProgram::applyShaderFloat(int32 uniformId, float32 value)
 	{
-		ASTRAL_LOG_ERROR("FilePath " + filePath + " couldn't be loaded");
-		shaderFile.close();
-		return;
+		gl::glUniform1f(uniformId, value);
 	}
-	std::string fileContents= "";
-	std::string line;
-	while(std::getline(shaderFile, line))
-	{
-		fileContents+= line + "\n";
-	}
-	shaderFile.close();
-	const char* charPointer= fileContents.c_str();
 
-	if(compileShaderSourceCode(charPointer, ID))
+	void ShaderProgram::applyShaderInt(int32 uniformId, int32 value)
 	{
-		ASTRAL_LOG_TRACE(filePath + " compiled successfully");
+		gl::glUniform1i(uniformId, value);
 	}
-	else
+
+	void ShaderProgram::applyShaderInt2(int32 uniformId, const glm::ivec2& value)
 	{
+		gl::glUniform2i(uniformId, value.x, value.y);
+	}
+
+	void ShaderProgram::applyShaderUInt(int32 uniformId, uint32 value)
+	{
+		gl::glUniform1ui(uniformId, value);
+	}
+
+	void ShaderProgram::applyShaderUInt2(int32 uniformId, const glm::uvec2& value)
+	{
+		gl::glUniform2ui(uniformId, value.x, value.y);
+	}
+
+	void ShaderProgram::applyShaderBool(int32 uniformId, bool value)
+	{
+		gl::glUniform1i(uniformId, value);
+	}
+
+	uint32 ShaderProgram::createProgram(Errors::GenericError& error) const
+	{
+		uint32 programID= gl::glCreateProgram();
+		error= (programID == 0) ? Errors::GenericError::FAILURE : Errors::GenericError::SUCCESS;
+		return programID;
+	}
+
+	uint32 ShaderProgram::createVertexShader(Errors::GenericError& error) const
+	{
+		uint32 shaderID= gl::glCreateShader(gl::GL_VERTEX_SHADER);
+		error= (shaderID == 0) ? Errors::GenericError::FAILURE : Errors::GenericError::SUCCESS;
+		return shaderID;
+	}
+
+	uint32 ShaderProgram::createFragmentShader(Errors::GenericError& error) const
+	{
+		uint32 shaderID= gl::glCreateShader(gl::GL_FRAGMENT_SHADER);
+		error= (shaderID == 0) ? Errors::GenericError::FAILURE : Errors::GenericError::SUCCESS;
+		return shaderID;
+	}
+
+	void ShaderProgram::initShader(Errors::GenericError& error)
+	{
+		Errors::GenericError programError= Errors::GenericError::INVALID;
+		Errors::GenericError vsError     = Errors::GenericError::INVALID;
+		Errors::GenericError fsError     = Errors::GenericError::INVALID;
+
+		programID= createProgram(programError);
+		if(programError != Errors::GenericError::SUCCESS)
+		{
+			ASTRAL_LOG_ERROR("Program creation failed");
+		}
+
+		vertexShaderID= createVertexShader(vsError);
+		if(vsError != Errors::GenericError::SUCCESS)
+		{
+			ASTRAL_LOG_ERROR("Vertex shader creation failed");
+		}
+
+		fragmentShaderID= createFragmentShader(fsError);
+		if(fsError != Errors::GenericError::SUCCESS)
+		{
+			ASTRAL_LOG_ERROR("Fragment shader creation failed");
+		}
+
+		error= std::max({ programError, vsError, fsError });
+
+		ASTRAL_ASSERTM(error == Errors::GenericError::SUCCESS, "Shader creation failed");
+	}
+
+	void ShaderProgram::compileShaderFromFilePath(const std::string& filePath, uint32 ID)
+	{
+		std::string fileContents;
+
+		Errors::GenericError error= getFileContent(filePath, fileContents);
+
+		if(error != Errors::GenericError::SUCCESS)
+		{
+			ASTRAL_LOG_ERROR("FilePath " + filePath + " couldn't be loaded");
+			return;
+		}
+
+		if(compileShaderSourceCode(fileContents, ID))
+		{
+			ASTRAL_LOG_TRACE(filePath + " compiled successfully");
+			return;
+		}
+
 		ASTRAL_LOG_TRACE(filePath + " failed to compile");
 	}
-}
 
-bool ShaderProgram::compileShaderSourceCode(const std::string& srcCode, uint32 ID)
-{
-	const char* charPointer= srcCode.c_str();
-	gl::glShaderSource(ID, 1, &charPointer, nullptr);
-	gl::glCompileShader(ID);
-
-	gl::GLint success= 0;
-	gl::glGetShaderiv(ID, gl::GL_COMPILE_STATUS, &success);
-
-	if(success == static_cast<gl::GLint>(gl::GL_FALSE))
+	bool ShaderProgram::compileShaderSourceCode(const std::string& srcCode, uint32 ID)
 	{
-		gl::GLint maxLength= 0;
-		gl::glGetShaderiv(ID, gl::GL_INFO_LOG_LENGTH, &maxLength);
-		std::vector<char> errorLog(maxLength);
-		gl::glGetShaderInfoLog(ID, maxLength, &maxLength, &errorLog[0]);
-		ASTRAL_LOG_DEBUG(&(errorLog[0]));
+		const char* charPointer= srcCode.c_str();
+		gl::glShaderSource(ID, 1, &charPointer, nullptr);
+		gl::glCompileShader(ID);
 
-		gl::glDeleteShader(ID);
-		return false;
+		gl::GLint success= 0;
+		gl::glGetShaderiv(ID, gl::GL_COMPILE_STATUS, &success);
+
+		if(success == static_cast<gl::GLint>(gl::GL_FALSE))
+		{
+			gl::GLint maxLength= 0;
+			gl::glGetShaderiv(ID, gl::GL_INFO_LOG_LENGTH, &maxLength);
+			std::vector<char> errorLog(maxLength);
+			gl::glGetShaderInfoLog(ID, maxLength, &maxLength, &errorLog[0]);
+			ASTRAL_LOG_DEBUG(&(errorLog[0]));
+
+			gl::glDeleteShader(ID);
+			return false;
+		}
+
+		return true;
 	}
-
-	return true;
-}
+} // namespace AstralRaytracer
