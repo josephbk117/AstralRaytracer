4160bc7bdfe2a166b9e29dcb1abf184e232a4e95
Joseph Kalathil <josephbk.117@hotmail.com>
2024-03-15 14:37:19 +0100

    brought all functions into AstralRaytracer namepsace

--- AstralRaytracerLib/src/Utils/Common.cpp
+++ AstralRaytracerLib/src/Utils/Common.cpp
@@ -1,145 +1,167 @@
 #include "Utils/Common.h"
 
+#include <fstream>
 #include <mimalloc-override.h> // redefines malloc etc.
 #include <mimalloc.h>
 #ifdef _WIN32
 	#include <mimalloc-new-delete.h>
 #endif
 
-std::string AstralRaytracer::toString(AssetType assetType)
+namespace AstralRaytracer
 {
-	switch(assetType)
+	std::string toString(AssetType assetType)
 	{
-		case AstralRaytracer::AssetType::PROJECT: return "Project";
-		case AstralRaytracer::AssetType::SCENE: return "Scene";
-		case AstralRaytracer::AssetType::TEXTURE: return "Texture";
-		case AstralRaytracer::AssetType::MATERIAL: return "Material";
-		case AstralRaytracer::AssetType::TRACEABLE: return "Traceable";
-		case AstralRaytracer::AssetType::INVALID:
-		default: return "";
-	}
-}
-
-std::string AstralRaytracer::toString(ResourceType resourceType)
-{
-	switch(resourceType)
-	{
-		case AstralRaytracer::ResourceType::IMAGE: return "Image";
-		case AstralRaytracer::ResourceType::MODEL: return "Model";
-		case AstralRaytracer::ResourceType::TEXT_FILE: return "Text";
-		case AstralRaytracer::ResourceType::FONT: return "Font";
-		case AstralRaytracer::ResourceType::INVALID:
-		default: return "";
-	}
-}
-
-const std::string& AstralRaytracer::getFileExtensionForAssetType(AssetType type)
-{
-	switch(type)
-	{
-		case AssetType::PROJECT: return FileExtensionForProject;
-		case AssetType::SCENE: return FileExtensionForScene;
-		case AssetType::TEXTURE: return FileExtensionForTexture;
-		case AssetType::MATERIAL: return FileExtensionForMaterial;
-		case AssetType::TRACEABLE: return FileExtensionForTraceable;
-		case AssetType::INVALID:
-		default: return invalidString;
-	}
-}
-
-const std::vector<std::string>& AstralRaytracer::getFileExtensionForResourceType(ResourceType type)
-{
-	switch(type)
-	{
-		case ResourceType::IMAGE: return fileExtensionForImages;
-		case ResourceType::MODEL: return fileExtensionForModels;
-		case ResourceType::TEXT_FILE: return fileExtensionForText;
-		case ResourceType::FONT: return fileExtensionForFont;
-		case ResourceType::INVALID:
-		default: return invalidVectorOfString;
-	}
-}
-
-AstralRaytracer::AssetType
-AstralRaytracer::getAssetTypeFromFilePath(const std::filesystem::path& filepath)
-{
-	if(!filepath.has_extension())
-	{
-		return AstralRaytracer::AssetType::INVALID;
+		switch(assetType)
+		{
+			case AssetType::PROJECT: return "Project";
+			case AssetType::SCENE: return "Scene";
+			case AssetType::TEXTURE: return "Texture";
+			case AssetType::MATERIAL: return "Material";
+			case AssetType::TRACEABLE: return "Traceable";
+			case AssetType::INVALID:
+			default: return "";
+		}
 	}
 
-	std::string fileExtension= filepath.extension().string();
-
-	if(fileExtension == AstralRaytracer::FileExtensionForProject)
+	std::string toString(ResourceType resourceType)
 	{
-		return AstralRaytracer::AssetType::PROJECT;
-	}
-	if(fileExtension == AstralRaytracer::FileExtensionForScene)
-	{
-		return AstralRaytracer::AssetType::SCENE;
-	}
-	if(fileExtension == AstralRaytracer::FileExtensionForTexture)
-	{
-		return AstralRaytracer::AssetType::TEXTURE;
-	}
-	if(fileExtension == AstralRaytracer::FileExtensionForMaterial)
-	{
-		return AstralRaytracer::AssetType::MATERIAL;
-	}
-	if(fileExtension == AstralRaytracer::FileExtensionForTraceable)
-	{
-		return AstralRaytracer::AssetType::TRACEABLE;
+		switch(resourceType)
+		{
+			case ResourceType::IMAGE: return "Image";
+			case ResourceType::MODEL: return "Model";
+			case ResourceType::TEXT_FILE: return "Text";
+			case ResourceType::FONT: return "Font";
+			case ResourceType::INVALID:
+			default: return "";
+		}
 	}
 
-	return AstralRaytracer::AssetType::INVALID;
-}
-
-AstralRaytracer::ResourceType
-AstralRaytracer::getResourceTypeFromFilePath(const std::filesystem::path& filepath)
-{
-	const std::string& extension= filepath.extension().string();
-
-	auto it= std::find(fileExtensionForImages.begin(), fileExtensionForImages.end(), extension);
-	if(it != fileExtensionForImages.end())
+	const std::string& getFileExtensionForAssetType(AssetType type)
 	{
-		return ResourceType::IMAGE;
+		switch(type)
+		{
+			case AssetType::PROJECT: return FileExtensionForProject;
+			case AssetType::SCENE: return FileExtensionForScene;
+			case AssetType::TEXTURE: return FileExtensionForTexture;
+			case AssetType::MATERIAL: return FileExtensionForMaterial;
+			case AssetType::TRACEABLE: return FileExtensionForTraceable;
+			case AssetType::INVALID:
+			default: return invalidString;
+		}
 	}
 
-	it= std::find(fileExtensionForModels.begin(), fileExtensionForModels.end(), extension);
-	if(it != fileExtensionForModels.end())
+	const std::vector<std::string>& getFileExtensionForResourceType(ResourceType type)
 	{
-		return ResourceType::MODEL;
+		switch(type)
+		{
+			case ResourceType::IMAGE: return fileExtensionForImages;
+			case ResourceType::MODEL: return fileExtensionForModels;
+			case ResourceType::TEXT_FILE: return fileExtensionForText;
+			case ResourceType::FONT: return fileExtensionForFont;
+			case ResourceType::INVALID:
+			default: return invalidVectorOfString;
+		}
 	}
 
-	it= std::find(fileExtensionForText.begin(), fileExtensionForText.end(), extension);
-	if(it != fileExtensionForText.end())
+	AssetType getAssetTypeFromFilePath(const std::filesystem::path& filepath)
 	{
-		return ResourceType::TEXT_FILE;
+		if(!filepath.has_extension())
+		{
+			return AssetType::INVALID;
+		}
+
+		std::string fileExtension= filepath.extension().string();
+
+		if(fileExtension == FileExtensionForProject)
+		{
+			return AssetType::PROJECT;
+		}
+		if(fileExtension == FileExtensionForScene)
+		{
+			return AssetType::SCENE;
+		}
+		if(fileExtension == FileExtensionForTexture)
+		{
+			return AssetType::TEXTURE;
+		}
+		if(fileExtension == FileExtensionForMaterial)
+		{
+			return AssetType::MATERIAL;
+		}
+		if(fileExtension == FileExtensionForTraceable)
+		{
+			return AssetType::TRACEABLE;
+		}
+
+		return AssetType::INVALID;
 	}
 
-	it= std::find(fileExtensionForFont.begin(), fileExtensionForFont.end(), extension);
-	if(it != fileExtensionForFont.end())
+	ResourceType getResourceTypeFromFilePath(const std::filesystem::path& filepath)
 	{
-		return ResourceType::FONT;
+		const std::string& extension= filepath.extension().string();
+
+		auto it= std::find(fileExtensionForImages.begin(), fileExtensionForImages.end(), extension);
+		if(it != fileExtensionForImages.end())
+		{
+			return ResourceType::IMAGE;
+		}
+
+		it= std::find(fileExtensionForModels.begin(), fileExtensionForModels.end(), extension);
+		if(it != fileExtensionForModels.end())
+		{
+			return ResourceType::MODEL;
+		}
+
+		it= std::find(fileExtensionForText.begin(), fileExtensionForText.end(), extension);
+		if(it != fileExtensionForText.end())
+		{
+			return ResourceType::TEXT_FILE;
+		}
+
+		it= std::find(fileExtensionForFont.begin(), fileExtensionForFont.end(), extension);
+		if(it != fileExtensionForFont.end())
+		{
+			return ResourceType::FONT;
+		}
+
+		return ResourceType::INVALID;
 	}
 
-	return ResourceType::INVALID;
-}
-
-AstralRaytracer::FileType
-AstralRaytracer::getFileTypeFromFilePath(const std::filesystem::path& filepath)
-{
-	const std::string& extension= filepath.extension().string();
-
-	if(getResourceTypeFromFilePath(filepath) != ResourceType::INVALID)
+	FileType getFileTypeFromFilePath(const std::filesystem::path& filepath)
 	{
-		return FileType::RESOURCE;
+		const std::string& extension= filepath.extension().string();
+
+		if(getResourceTypeFromFilePath(filepath) != ResourceType::INVALID)
+		{
+			return FileType::RESOURCE;
+		}
+
+		if(getAssetTypeFromFilePath(filepath) != AssetType::INVALID)
+		{
+			return FileType::ASSET;
+		}
+
+		return FileType::INVALID;
 	}
 
-	if(getAssetTypeFromFilePath(filepath) != AssetType::INVALID)
+	Errors::GenericError
+	getFileContent(const std::filesystem::path& filepath, std::string& outFileContent)
 	{
-		return FileType::ASSET;
-	}
+		const std::string filePathString= filepath.string();
 
-	return FileType::INVALID;
-}
+		std::ifstream shaderFile(filePathString.c_str());
+		if(shaderFile.fail())
+		{
+			shaderFile.close();
+			return Errors::GenericError::FAILURE;
+		}
+		std::string fileContents= "";
+		std::string line;
+		while(std::getline(shaderFile, line))
+		{
+			fileContents+= line + "\n";
+		}
+		shaderFile.close();
+		return Errors::GenericError::SUCCESS;
+	}
+} // namespace AstralRaytracer
