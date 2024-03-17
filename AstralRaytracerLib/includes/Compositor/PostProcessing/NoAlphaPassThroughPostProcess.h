d8580b77438eda1dbd8aab5096377c97014f3b61
Joseph Kalathil <josephbk.117@hotmail.com>
2024-03-17 16:44:36 +0100

    added no alpha pass through shader

---
+++ AstralRaytracerLib/includes/Compositor/PostProcessing/NoAlphaPassThroughPostProcess.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "PostProcessing.h"
+
+namespace AstralRaytracer
+{
+	class NoAlphaPassThroughPostProcessing: public PostProcessing
+	{
+		public:
+			const std::string& getName() const override;
+
+			void init() override;
+
+			void processImage(
+					DrawingPanel&        drawPanel,
+					const RenderTexture& renderTexture,
+					gl::GLuint           imageTexture
+			) const override;
+		protected:
+			const char* const getFragmentShaderSrcCode() const override;
+	};
+} // namespace AstralRaytracer
---
+++ AstralRaytracerLib/src/Compositor/PostProcessing/NoAlphaPassThroughPostProcess.cpp
@@ -0,0 +1,53 @@
+#include "Compositor/PostProcessing/NoAlphaPassThroughPostProcess.h"
+
+const std::string& AstralRaytracer::NoAlphaPassThroughPostProcessing::getName() const
+{
+	static const std::string name("Vertical Gauss Blur");
+	return name;
+}
+
+void AstralRaytracer::NoAlphaPassThroughPostProcessing::init() 
+{
+	m_shaderProgram.compileShadersFromSrcCode(getVertexShaderSrcCode(), getFragmentShaderSrcCode());
+	m_shaderProgram.linkShaders();
+
+	m_shaderProgram.use();
+	m_shaderProgram.unuse();
+}
+
+void AstralRaytracer::NoAlphaPassThroughPostProcessing::processImage(
+		DrawingPanel&        drawPanel,
+		const RenderTexture& renderTexture,
+		gl::GLuint           imageTexture
+) const
+{
+	renderTexture.bind();
+	gl::glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	gl::glClear(gl::ClearBufferMask::GL_COLOR_BUFFER_BIT);
+
+	m_shaderProgram.use();
+	drawPanel.setTextureID(imageTexture);
+	drawPanel.draw();
+	m_shaderProgram.unuse();
+
+	renderTexture.unbind();
+}
+
+const char* const
+AstralRaytracer::NoAlphaPassThroughPostProcessing::getFragmentShaderSrcCode() const
+{
+	return R"SHADER(
+						#version 330 core
+						in vec2				textureUV;
+						in vec3				worldPos;
+						out vec4			color;
+						uniform sampler2D	inputTexture0;
+
+						void main()
+						{
+							// Sample the texture
+							vec3 texColor = texture(inputTexture0, textureUV).rgb;
+							color = vec4(texColor, 1.0);
+						}
+				)SHADER";
+}
